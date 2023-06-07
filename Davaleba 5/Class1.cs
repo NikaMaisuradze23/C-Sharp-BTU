4f1f4412f0f76d5ab2194f639381efa8b765a314
Nika Maisuradze <tudula6@gmail.com>
2023-06-07 22:26:39 +0400

    test

---
+++ Davaleba 5/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAVALEBA14
+{
+    internal class Class1
+    {
+        string[] saxelebi = { "ბარისახო", "თბილისი", "ქანდა", "ბაკურიანი" };
+
+        //var shedegi = from cvladi in saxelebi where cvladi.StartsWith("ბ") select cvladi;
+
+        
+
+    }
+}
